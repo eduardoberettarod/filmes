3d07e70968ca53c96b04800a27c15eae78bf70c9
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-06-03 22:15:54 -0300

    aulas

---
+++ app.js
@@ -0,0 +1,26 @@
+// JSON - javascript object notation
+
+let filme1 = {
+    título: "Jurassic Park 0",
+    foto: "filme1.webp",
+    avaliacao: "10/10",
+    duracao: "1h50m"
+}
+let filme2 = {
+    título: "Jurassic Park 0",
+    foto: "filme1.webp",
+    avaliacao: "10/10",
+    duracao: "1h50m"
+}
+let filme3 = {
+    título: "Jurassic Park 0",
+    foto: "filme1.webp",
+    avaliacao: "10/10",
+    duracao: "2h00m"
+}
+let filme4 = {
+    título: "Pecadores",
+    foto: "filme4.jpg",
+    avaliacao: "9/10",
+    duracao: "2h03m"
+}
