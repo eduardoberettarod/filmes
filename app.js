912cda22e0e2348d73a1ace243e06ce33c351912
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-06-10 22:11:47 -0300

    .

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
 // JSON - javascript object notation
-
-let filme1 = {
+let todosOsFilmes = [
+{
     título: "Jurassic Park 0",
     foto: "filme1.webp",
     avaliacao: "10/10",
     duracao: "1h50m"
-}
-let filme2 = {
-    título: "Jurassic Park 0",
-    foto: "filme1.webp",
+},
+{
+    título: "Karate Kid Lendas",
+    foto: "filme2.jpg",
     avaliacao: "10/10",
-    duracao: "1h50m"
-}
-let filme3 = {
-    título: "Jurassic Park 0",
-    foto: "filme1.webp",
+    duracao: "1h34m"
+},
+{
+    título: "Lilo & Stitch",
+    foto: "filme3.jpg",
     avaliacao: "10/10",
-    duracao: "2h00m"
-}
-let filme4 = {
+    duracao: "1h48m"
+},
+{
     título: "Pecadores",
     foto: "filme4.jpg",
     avaliacao: "9/10",
     duracao: "2h03m"
+},
+{
+    título: "Trem-Bala",
+    foto: "filme5.jpg",
+    avaliacao: "4/10",
+    duracao: "2h06m"
 }
+]
+function fnMontarCartao(filmeAtual){
+    document.querySelector(".lista-filmes").innerHTML += `
+      <div class="card-filme">
+        <img src="img/${filmeAtual.foto}">
+        <h3>${filmeAtual.título}</h3>
+        <span>⭐ ${filmeAtual.avaliacao}</span>
+        <br>
+        <span>${filmeAtual.duracao}</span>
+      </div>
+    `
+  }
+  
+// DOM - Document Object Model
+
+todosOsFilmes.forEach((filmeAtual) => {
+  document.querySelector(".lista-filmes").innerHTML += `
+    <div class="card-filme">
+      <img src="img/${filmeAtual.foto}">
+      <h3>${filmeAtual.titulo}</h3>
+      <span>⭐ ${filmeAtual.avaliacao}</span>
+    </div>
+  `
+})
+
+
+  
