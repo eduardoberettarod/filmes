53122b30fe9dcc2e36f60ca4cf72f0bd79d55c91
eduardoberettarod <eduardo.berettarod@gmail.com>
2025-06-12 21:41:08 -0300

    exibição de filmes na base de dados

--- app.js
+++ app.js
@@ -1,59 +1,16 @@
-// JSON - javascript object notation
-let todosOsFilmes = [
-{
-    título: "Jurassic Park 0",
-    foto: "filme1.webp",
-    avaliacao: "10/10",
-    duracao: "1h50m"
-},
-{
-    título: "Karate Kid Lendas",
-    foto: "filme2.jpg",
-    avaliacao: "10/10",
-    duracao: "1h34m"
-},
-{
-    título: "Lilo & Stitch",
-    foto: "filme3.jpg",
-    avaliacao: "10/10",
-    duracao: "1h48m"
-},
-{
-    título: "Pecadores",
-    foto: "filme4.jpg",
-    avaliacao: "9/10",
-    duracao: "2h03m"
-},
-{
-    título: "Trem-Bala",
-    foto: "filme5.jpg",
-    avaliacao: "4/10",
-    duracao: "2h06m"
-}
-]
-function fnMontarCartao(filmeAtual){
-    document.querySelector(".lista-filmes").innerHTML += `
+async function fnPegarFilmes(){
+  let filmes = await fetch("dados-filmes.json")
+
+  let filmesTratados = await filmes.json()
+  filmesTratados.slice(0,4).forEach((filmeAtual,) => {
+      document.querySelector(".lista-filmes").innerHTML += `
       <div class="card-filme">
-        <img src="img/${filmeAtual.foto}">
-        <h3>${filmeAtual.título}</h3>
-        <span>⭐ ${filmeAtual.avaliacao}</span>
-        <br>
-        <span>${filmeAtual.duracao}</span>
+          <img src="${filmeAtual.foto}">
+          <h3>${filmeAtual.titulo}</h3>
+          <span>⭐ ${filmeAtual.avaliacao}</span>
       </div>
-    `
-  }
-  
-// DOM - Document Object Model
+      `
+  })
+}
 
-todosOsFilmes.forEach((filmeAtual) => {
-  document.querySelector(".lista-filmes").innerHTML += `
-    <div class="card-filme">
-      <img src="img/${filmeAtual.foto}">
-      <h3>${filmeAtual.titulo}</h3>
-      <span>⭐ ${filmeAtual.avaliacao}</span>
-    </div>
-  `
-})
-
-
-  
+fnPegarFilmes()
---
+++ app2.js
@@ -0,0 +1,16 @@
+async function fnPegarFilmes(){
+    let filmes = await fetch("dados-filmes.json")
+    let filmesTratados = await filmes.json()
+    filmesTratados.forEach((filmeAtual) => {
+        document.querySelector(".lista-filmes").innerHTML += `
+        <div class="card-filme">
+            <img src="${filmeAtual.foto}">
+            <h3>${filmeAtual.titulo}</h3>
+            <span>⭐ ${filmeAtual.avaliacao}</span>
+        </div>
+        `
+    })
+}
+
+fnPegarFilmes()
+
